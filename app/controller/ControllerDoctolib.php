d22378fe91c8532ff2f80ca2d6a122cef65398b0
JiaoZihang <jiaozihang1020@gmail.com>
2023-05-18 15:17:59 +0200

    first

---
+++ app/controller/ControllerDoctolib.php
@@ -0,0 +1,14 @@
+<?php
+
+class ControllerDoctolib{
+    // --- page d'acceuil
+    public static function doctolibAccueil() {
+        include 'config.php';
+        $vue = $root . '/app/view/viewDoctolibAccueil.php';
+        if (DEBUG) {
+            echo ("ControllerDoctolib : doctolibAccueil : vue = $vue");
+        }
+        require ($vue);
+    }
+}
+
